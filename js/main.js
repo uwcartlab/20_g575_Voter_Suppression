285ae1804cf5f71275c30e263451b1409393f141
mbiehlgis <60198329+mbiehlgis@users.noreply.github.com>
2020-04-24 22:06:26 -0500

    Potential Template Created, Basemap of US Appended
    - Also added accordion legend we could use

---
+++ js/main.js
@@ -0,0 +1,63 @@
+//begin script when window loads
+window.onload = setMap();
+
+//set up choropleth map
+function setMap(){
+
+    //map frame dimensions
+    var width = 960,
+        height = 460;
+
+    //create new svg container for the map
+    var map = d3.select("body")
+        .append("svg")
+        .attr("class", "map")
+        .attr("width", width)
+        .attr("height", height);
+
+        //create custom d3 albers projection specific for the US
+        var projection = d3.geoAlbersUsa()
+            .scale(950)
+            .translate([width / 2, height / 2]);
+
+        var path = d3.geoPath()
+            .projection(projection);
+
+    //use Promise.all to parallelize asynchronous data loading
+    var promises = [];
+    promises.push(d3.csv('data/State_Voting_Laws.csv')); //Load CSV attributes
+    promises.push(d3.json("data/USAFinalProjectTopo.json"));    //load choropleth spatial data
+    promises.push(d3.json("data/GreatLakesTopo.json"));         //loads great lakes layers
+    Promise.all(promises).then(callback);
+
+    function callback(data){
+      	csvData = data[0];
+      	usa = data[1];
+        bigLakes = data[2];
+
+        //translate US TopoJSON
+        var usaStates = topojson.feature(usa, usa.objects.USAFinalProject).features,
+            greatLakes = topojson.feature(bigLakes, bigLakes.objects.GreatLakes);
+            //examine the results
+            console.log(greatLakes);
+            console.log(usaStates);
+
+            //add states to map
+            var states = map.selectAll(".states")
+                .data(usaStates)
+                .enter()
+                .append("path")
+                .attr("class", function(d){
+                    return "State: " + d.properties.name;
+                })
+                .attr("d", path);
+
+            //add great lakes to map
+            var lakes = map.append("path")
+                .datum(greatLakes)
+                .attr("class", "lakes")
+                .attr("d", path);
+
+    };
+
+};
