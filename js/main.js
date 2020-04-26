ec9b0f9c1351eedcf7b91231ddaa38545e659961
mbiehlgis <60198329+mbiehlgis@users.noreply.github.com>
2020-04-26 06:59:21 -0500

    Added Basic Highlighting to Map
    - State borders overlap others on highlight
    - changed join from name to abbreviation

--- js/main.js
+++ js/main.js
@@ -1,6 +1,10 @@
 //begin script when window loads
 window.onload = setMap();
 
+// variables for data join from csv
+var attrArray = ["Online Reg Implement Yr", "Early Voting Status", "Voter ID Requirement", "Election Day Vote Centers", "Rights Lost to Felons", "Incorrectly Cast Provisional Vote"];
+var expressed = attrArray[0]; //initial attribute
+
 //set up choropleth map
 function setMap(){
 
@@ -38,35 +42,26 @@ function setMap(){
         //translate US TopoJSON
         var usaStates = topojson.feature(usa, usa.objects.USAFinalProject).features,
             greatLakes = topojson.feature(bigLakes, bigLakes.objects.GreatLakes);
-            //examine the results
-            console.log(greatLakes);
-            console.log(usaStates);
+
         // join csv data to GeoJSON data
         usaStates = joinData(usaStates, csvData);
 
-            //add states to map
-            var states = map.selectAll(".states")
-                .data(usaStates)
-                .enter()
-                .append("path")
-                .attr("class", function(d){
-                    return "State: " + d.properties.name;
-                })
-                .attr("d", path);
+        var colorScale = makeColorScale(csvData)
 
-            //add great lakes to map
-            var lakes = map.append("path")
-                .datum(greatLakes)
-                .attr("class", "lakes")
-                .attr("d", path);
+        //add enumeration units to the map
+        setEnumerationUnits(usaStates, map, path, colorScale);
+
+        //add great lakes to map
+        var lakes = map.append("path")
+            .datum(greatLakes)
+            .attr("class", "lakes")
+            .attr("d", path);
 
     };
 
 };
 
 function joinData(usaStates, csvData){
-    // variables for data join from csv
-    var attrArray = ["Online Reg Implement Yr", "Early Voting Status", "Voter ID Requirement", "Election Day Vote Centers", "Rights Lost to Felons", "Incorrectly Cast Provisional Vote"];
     // assign csv attributes to GeoJSON with each loop
     for (var i=0; i<csvData.length; i++){
         // index states
@@ -77,7 +72,7 @@ function joinData(usaStates, csvData){
         // loop through GeoJSON states to find correct one
         for (var a=0; a<usaStates.length; a++){
             var geojsonProps = usaStates[a].properties,
-                geojsonKey = geojsonProps.name;
+                geojsonKey = geojsonProps.StateAbb;
 
             // conditional statement transferring data when names match
             if (geojsonKey == csvKey){
@@ -93,4 +88,100 @@ function joinData(usaStates, csvData){
         };
     };
     return usaStates;
+};
+
+//function to create color scale generator
+function makeColorScale(data){
+
+    // DARK BLUE COLOR SCALE
+    var colorClasses = [
+        "#f1eef6",
+        "#bdc9e1",
+        "#74a9cf",
+        "#2b8cbe",
+        "#045a8d"
+    ];
+
+        // NATURAL BREAKS SCALE
+    //create color scale generator
+    var colorScale = d3.scaleThreshold()
+    .range(colorClasses);
+
+    //build array of all values of the expressed attribute
+    var domainArray = [];
+    for (var i=0; i<data.length; i++){
+    var val = parseFloat(data[i][expressed]);
+    domainArray.push(val);
+    };
+
+    //cluster data using ckmeans clustering algorithm to create natural breaks
+    var clusters = ss.ckmeans(domainArray, 5);
+    //reset domain array to cluster minimums
+    domainArray = clusters.map(function(d){
+    return d3.min(d);
+    });
+    //remove first value from domain array to create class breakpoints
+    domainArray.shift();
+
+    //assign array of last 4 cluster minimums as domain
+    colorScale.domain(domainArray);
+
+    return colorScale;
+};
+
+function setEnumerationUnits(usaStates, map, path, colorScale){
+    //add states to map
+    var states = map.selectAll(".states")
+        .data(usaStates)
+        .enter()
+        .append("path")
+        .attr("class", function(d){
+            return "State: " + d.properties.StateAbb;
+        })
+        .attr("d", path)
+          .on("mouseover", function(d){
+              highlight(d.properties);
+          })
+          .on("mouseout", function(d){
+              dehighlight(d.properties);
+          });
+
+      //dehighlight for stroke
+      var desc = states.append("desc")
+          .text('{"stroke": "#fff", "stroke-width": ".1px"}');
+
+      //alternate dehighlight for fill
+      // var desc = states.append("desc")
+      //     .text('{"fill": "blue"}');
+};
+
+
+function highlight(props){
+                //change STROKE highlight method
+    var selected = d3.selectAll("." + props.StateAbb.replace(/\s+/g, ''))
+        .style("stroke", "#ffffcc") //highlight color
+        .style("stroke-width", "2px"); //highlight width
+};
+
+//function to reset the element style on mouseout
+function dehighlight(props){
+              // STROKE DEHIGHLIGHT
+  var selected = d3.selectAll("." + props.StateAbb.replace(/\s+/g, ''))
+        .style("stroke", function(){
+            return getStyle(this, "stroke")
+        })
+        .style("stroke-width", function(){
+            return getStyle(this, "stroke-width")
+        });
+
+    function getStyle(element, styleName){
+        var styleText = d3.select(element)
+            .select("desc")
+            .text();
+
+        var styleObject = JSON.parse(styleText);
+
+        return styleObject[styleName];
+        };
+
 };
